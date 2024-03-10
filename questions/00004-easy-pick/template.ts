599274a0aa33de4218d1cabea9e01ba0e9d867ca
Aika Takada <aktsm0syn0pgmail.com>
2024-03-10 15:17:04 +0900

    ✨  回答を追加する, 00004-easy-pick

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,7 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [key in K]: T[key]
+}
+
+// K extnds keyof T → Tだけだと型が広すぎるため、限定させる
+// [key in K] → K = 'title' | 'completed' のようなUnionを持っている為、それをバラしてkeyに格納する。
+// T[key] → key = 'title'、key =’complete’になっている為、T[key]としてあげる
