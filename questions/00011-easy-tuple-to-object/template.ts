c85428741c08f6e8c5d9aaec5446151063ce9ef9
Aika Takada <aktsm0syn0pgmail.com>
2024-03-23 15:08:48 +0900

    ✨ 回答を追加する, 00011-easy-tuple-to-object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,12 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly PropertyKey[]> = {
+  [K in T[number]]: K
+}
+
+// extends readonly PropertyKey[]
+// 引数をPropertyKey(string | number | symbol)に絞る
+
+// T[number]
+// index access を使用してタプルの中身を取り出す
+
+// [K in T[number]]: K
+// タプルの中身をKに代入し、keyとvalueに設定する
