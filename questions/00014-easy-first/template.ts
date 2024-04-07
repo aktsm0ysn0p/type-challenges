a0481e4c2eecad0ec69ae5c14c2fb3195350d3de
Aika Takada <aktsm0syn0pgmail.com>
2024-04-07 17:23:02 +0900

    ✨ 回答を追加する, 00014-easy-first

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,9 @@
-type First<T extends any[]> = any
+type First<T extends any[]> = T extends [] ? never : T[0]
+
+// 1. T extends []
+// →空配列を受け取った場合、neverにする必要がある為、Conditional Type(T extends U ? X : Y)を使用して条件式を使う
+
+// 他の解答例：
+// type First<T extends any[]> = T['length'] extends 0 ? never : T[0]
+// 1. T['length'] extends 0
+// →Tの配列の長さをみる。空だった場合０になる為、extends 0としている。
